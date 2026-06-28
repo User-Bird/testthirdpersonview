94fa3f1dc55400938295333bd64e69348bc4d571
User-Bird <faridboualam06@gmail.com>
2026-06-28 13:09:12 +0100

    basic movement but not perfect camera bug

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,7 +20,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	
 	# 2. THE FIX: Calculate direction based on where the CameraPivot is looking
-	var direction := (camera_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (camera_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
 		velocity.x = direction.x * SPEED
